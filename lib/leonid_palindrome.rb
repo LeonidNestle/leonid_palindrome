c8ac481e202bded93981529e5de0a0650d65b081
leonid <leonidnestle@gmail.com>
2019-08-05 16:14:02 -0400

    initialize repository

---
+++ lib/leonid_palindrome.rb
@@ -0,0 +1,6 @@
+require "leonid_palindrome/version"
+
+module LeonidPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/leonid_palindrome/version.rb
@@ -0,0 +1,3 @@
+module LeonidPalindrome
+  VERSION = "0.1.0"
+end
