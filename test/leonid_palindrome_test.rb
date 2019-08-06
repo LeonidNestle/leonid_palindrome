0930d9c8466f3561c117cb4e92997efb79f28697
leonid <leonidnestle@gmail.com>
2019-08-06 15:31:48 -0400

    added stuff

--- test/leonid_palindrome_test.rb
+++ test/leonid_palindrome_test.rb
@@ -1,11 +1,38 @@
 require "test_helper"
 
 class LeonidPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::LeonidPalindrome::VERSION
+  #def test_that_it_has_a_version_number
+  #  refute_nil ::LeonidPalindrome::VERSION
+  #end
+
+  #def test_it_does_something_useful
+  #  assert true
+  #end
+
+  def test_non_palindrome
+    refute "apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
   end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    skip
+  end
+
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam".palindrome?
+  end
+
+  def test_letters
+    assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
+  end
+
+
 end
